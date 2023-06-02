2d292885f31b0ac5ac9af9747049aacb1b3e95f9
Rither Tandazo <rither5711@gmail.com>
2023-06-01 21:32:54 -0500

    img Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'wave-form': "url('/src/img/wave-white.png')",
-        'wave-patterns-dots': "url('/src/img/pattern-white-dots.png')",
-        'wave-pink': "url('/src/img/wave-pink.png')",
-        'character-mario': "url('/src/img/character-l.png')",
-        'patterns-ch-red': "url('/src/img/pattern-characters-red.png')",
-        'patterns-yellow-dots': "url('/src/img/pattern-yellow-dots.png')",
-      }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "wave-form": "url('/src/img/wave-white.png')",
+                "wave-patterns-dots": "url('/src/img/pattern-white-dots.png')",
+                "wave-pink": "url('/src/img/wave-pink.png')",
+                "character-mario": "url('/src/img/character-l.png')",
+                "patterns-ch-red": "url('/src/img/pattern-characters-red.png')",
+                "patterns-yellow-dots":
+                    "url('/src/img/pattern-yellow-dots.png')",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
