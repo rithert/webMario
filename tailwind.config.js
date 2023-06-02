52f097345e450e2c6696f3ee58b42526c61314c7
Rither Tandazo <rither5711@gmail.com>
2023-06-02 00:05:15 -0500

    img Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 "wave-form": "url('/src/img/wave-white.png')",
                 "wave-patterns-dots": "url('/src/img/pattern-white-dots.png')",
                 "wave-pink": "url('/src/img/wave-pink.png')",
-                "character-mario": "url('/src/img/character-l.png')",
+                /* "character-mario": "url('/src/img/character-l.png')", */
                 "patterns-ch-red": "url('/src/img/pattern-characters-red.png')",
                 "patterns-yellow-dots":
                     "url('/src/img/pattern-yellow-dots.png')",
